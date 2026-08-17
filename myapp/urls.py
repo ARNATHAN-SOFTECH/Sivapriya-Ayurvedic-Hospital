aaf632aad56846c3eae84de3b3a3da4dd0d9193c
Ardra <ardrapb201@gmail.com>
2026-08-17 14:38:01 +0530

    added treatment page

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('treatments/', views.treatments, name='treatments'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
+def treatments(request):
+    return render(request, 'treatments.html')
