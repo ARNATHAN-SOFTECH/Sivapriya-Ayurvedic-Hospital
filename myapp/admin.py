60c0190ee8827c8ff690453a87571719f8a4cb94
Amina <amina.ib.rz01@gmail.com>
2026-08-20 14:54:24 +0530

    Created FAQ

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from .models import Doctor
 from .models import Gallery
+from .models import FAQCategory, FAQ
+
+
+@admin.register(FAQCategory)
+class FAQCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'order')
+    ordering = ('order', 'name')
+
+
+@admin.register(FAQ)
+class FAQAdmin(admin.ModelAdmin):
+    list_display = ('question', 'category', 'is_active', 'order')
+    list_filter = ('category', 'is_active')
+    search_fields = ('question', 'answer')
+    ordering = ('category', 'order')
 
 @admin.register(Gallery)
 class GalleryAdmin(admin.ModelAdmin):
